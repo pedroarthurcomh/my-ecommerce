aa30fad318444b3715b7316c3af628b254cb3a15
Pedro Barros <am.pedrobarros@outlook.com>
2025-10-08 15:06:08 -0300

    fix: add product title resolver and update routing titles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: '<router-outlet></router-outlet>',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('my-ecommerce');
+
+  private _router = inject(Router);
+  private _activatedRoute = inject(ActivatedRoute);
+  private _titleService = inject(Title);
+
+  ngOnInit(): void {
+    this._router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route = this._activatedRoute;
+
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+
+          return route.snapshot.title;
+        }),
+      )
+      .subscribe((title: string | undefined) => {
+        if (title) this._titleService.setTitle(title);
+        else this._titleService.setTitle('E-commerce');
+      });
+  }
 }
