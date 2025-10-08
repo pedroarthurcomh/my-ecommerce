27192406a9ed6f936d1cd9bf6c06c99d1bfa0655
Pedro Barros <am.pedrobarros@outlook.com>
2025-10-08 01:10:51 -0300

    fix: translate products.ts from ES Modules to CommonJs

--- api/products.ts
+++ api/products.ts
@@ -1,29 +1,38 @@
+const path = require('path');
+const fs = require('fs');
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import productsData from './_data.json';
 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
+module.exports = (request: VercelRequest, response: VercelResponse) => {
+  try {
+    const jsonFilePath = path.join(__dirname, '_data.json');
+    const fileContents = fs.readFileSync(jsonFilePath, 'utf8');
+    const jsonData = JSON.parse(fileContents);
 
-  const { category, id } = request.query;
-
-  let products = productsData.products;
-
-  if (id) {
-    const product = products.find((p) => p.id === id);
-    if (product) {
-      return response.status(200).json(product);
+    if (!jsonData.products) {
+      throw new Error("A chave 'products' não foi encontrada no arquivo _data.json.");
     }
-    return response.status(404).json({ error: 'Product not found' });
+
+    const { category, id } = request.query;
+    let products = jsonData.products;
+
+    if (id && !Array.isArray(id)) {
+      const product = products.find((p: any) => p.id === id);
+      if (product) {
+        return response.status(200).json(product);
+      }
+      return response.status(404).json({ error: 'Product not found' });
+    }
+
+    if (category && typeof category === 'string') {
+      products = products.filter((p: any) => p.category === category);
+    }
+
+    return response.status(200).json(products);
+  } catch (error: any) {
+    console.error('ERRO NA FUNÇÃO SERVERLESS:', error);
+    return response
+      .status(500)
+      .json({ message: 'Ocorreu um erro no servidor.', details: error.message });
   }
-
-
-  if (category && typeof category === 'string') {
-    products = products.filter((p) => p.category === category);
-  }
-
-
-  response.status(200).json(products);
-}
+};
